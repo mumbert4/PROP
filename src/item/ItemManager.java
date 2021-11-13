b352d8fef64fd98e1c90ad8a3c065ff6a1cb6566
Aftab <aftab.ahmed.choudhry@estudiantat.upc.edu>
2021-11-13 19:10:07 +0100

    Esquelet de Item i ItemManager

---
+++ src/item/ItemManager.java
@@ -0,0 +1,29 @@
+package item;
+import java.util.*;
+
+public class ItemManager{
+
+    Map<Integer, Item> items;
+
+    public ItemManager(){
+        items = new HashMap<Integer, Item>();
+    }
+
+    boolean existItem(int id){
+        return items.containsKey(id);
+    }
+
+    void createItem(int id, String name, ArrayList<Integer> attributes){
+        if(existItem(id)) System.out.println("The item with id: " +id+" already exists");
+        else {
+            Item item = new Item(id, name, attributes);
+            items.put(id, item);
+        }
+    }
+
+    void eliminarItem(int id) {
+        if(!existItem(id)) System.out.println("The item with id: " +id+" does not exist");
+        else items.remove(id);
+    }
+
+}
