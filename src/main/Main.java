a4901dc5d357eec7f5d458fe3dba63367e751240
Miquel <miquel.umbert.bosch@gmail.com>
2021-11-16 17:08:51 +0100

    merge miquel controlador usuaris

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,7 @@
 package main;
 
-//
-//<<<<<<< HEAD
-//import userManager.userManager;
-//import activeUser.activeUser;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        userManager manager = userManager.getInstance();
-//        activeUser user = new activeUser("Miquel", "1234");
-//        manager.addUser(user);
-//        manager.createReview("Miquel",16, 5.5, "Ha estat molt be");
-//        manager.createReview("Miquel",16, 5.5, "Ha estat molt be");
-//        System.out.println("Usuari Miquel ha fet: " + manager.numReviews("Miquel")+ " reviews en total");
-//=======
+
+
 import data.CtrlDades;
 import java.util.LinkedList;
 import java.util.List;
