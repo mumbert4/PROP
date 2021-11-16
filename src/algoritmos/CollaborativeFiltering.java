93493b7862a9dfe3b0ca3c2930014be58e4c4a32
marta.granero.i <marta.granero.i@est.fib.upc.edu>
2021-11-16 19:49:07 +0100

    algorismes

---
+++ src/algoritmos/CollaborativeFiltering.java
@@ -0,0 +1,13 @@
+package algoritmos;
+
+import item.Item;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class CollaborativeFiltering implements RecommendationSystem {
+
+    public ArrayList<Item> calculate(Map<Integer, Map<Integer ,Double>> mapDistances) {
+        return null;
+    }
+
+}
